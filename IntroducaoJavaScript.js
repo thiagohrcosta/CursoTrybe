b6aa1fb9bc0410953c368e474387a454808cda1c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-13 23:36:06 -0300

    Introdução ao Javascript

---
+++ IntroducaoJavaScript.js
@@ -0,0 +1,34 @@
+// Curso de Preparação - TRYBE
+// O que é JavaScritp?
+
+/*
+
+    O Java foi lançado em 1991, uma das maiores linguagens do mercado atual, 
+    ano no qual também foi criado o HTML. Já em 1994 a NETSCAPE foi criada lançando
+    em meados de 1995 o Javascript. Posteriormente foi lançado um applets do Java, e 
+    o primeiro nome seria MOCHA. Contudo, tal nome não ganhou força, passando a ser 
+    cogitado LiveScript,o que também não foi usado, finalizando com o nome de JavaScript.
+    
+    O sucesso do JavaScript foi automático.
+
+    O site UOL por exemplo é carregado com JavaScript, tendo o seu conteúdo carregado 
+    conforme a visualização do usuário. 
+
+    Ao desativar o conteúdo Javascript do site UOL, vários dos conteúdos não serão carregados,
+    deformando o site. 
+
+    O Google também utiliza Javascript, que uma vez desabilitado via inspeção de elementos, 
+    será carregado de forma diferente, desabilitando, por exemplo, o autocomplete, contudo
+    ele por possuir uma versão específica para versão sem Javascript o site não perderá
+    grandes funcionalidades. 
+    
+    Outro site interessante para o teste é o Youtube, que ao ter o Javascript desabilitado
+    não carregará as publicidades, não havendo também um carregamento assíncrono. Não haverá
+    também o carregamento das informações do vídeo.
+    
+    Já outro grande exemplo é o G1, que com o Javascript será possível efetuar alterações 
+    na página. 
+
+
+
+*/
