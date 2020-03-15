cdc826e64d744e18fe2c3301da28813b8c269672
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-15 10:05:13 -0300

    Strings

---
+++ strings.js
@@ -0,0 +1,18 @@
+
+// Strings concatenadas
+
+var primeiroNome = "João";
+var segundoNome = "da Silva";
+
+var nomeCompleto = primeiroNome + segundoNome;
+
+console.log(nomeCompleto);
+
+nomeCompleto = primeiroNome + " " + segundoNome;
+console.log(nomeCompleto);
+
+var mensagem = "Vem pra Trybe!"
+console.log(mensagem.length);
+console.log(mensagem[0]);
+
+
