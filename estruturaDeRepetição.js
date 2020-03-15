2d33dcca43d39b83562b140f84f1fd8d4d13ac2d
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-15 10:26:26 -0300

    Estrutura de repetição

---
+++ estruturaDeRepetição.js
@@ -0,0 +1,15 @@
+
+var numero = 10;
+
+for (var contador = 1; contador <= 9; contador++){
+    console.log(numero * contador);
+}
+
+
+var listaDeNomes = ["Joana", "Maria", "Lucas"];
+
+for (var indice = 0; indice < listaDeNomes.length; indice++){
+    var mensagem = "Boas vindas " + listaDeNomes[indice] + " !";
+    console.log(mensagem); 
+}
+
